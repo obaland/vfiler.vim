8d77fad4b3d2f3d197ee9bfb05b616417e5ccfa6
th.obaland@gmail.com <th.obaland@gmail.com>
2022-02-04 09:15:18 +0900

    Update workflows.

--- tests/vfiler/sort_spec.lua
+++ tests/vfiler/sort_spec.lua
@@ -1,20 +1,35 @@
 local sort = require('vfiler/sort')
 
+local sort_types = {
+  'extension',
+  'name',
+  'size',
+  'time',
+  'Extension',
+  'Name',
+  'Size',
+  'Time',
+}
+
 describe('API', function()
   it('types', function()
-    local expected_types = {
-      'extension',
-      'name',
-      'size',
-      'time',
-      'Extension',
-      'Name',
-      'Size',
-      'Time',
-    }
     local types = sort.types()
-    for i = 1, #expected_types do
-      assert.is_equal(expected_types[i], types[i])
+    for i = 1, #sort_types do
+      assert.is_equal(sort_types[i], types[i])
     end
+  end)
+
+  -- test "get" function
+  for _, type in ipairs(sort_types) do
+    it('get type: ' .. type, function()
+      local comp = sort.get(type)
+      assert.is_not_nil(comp)
+    end)
+  end
+
+  it('set', function()
+    sort.set('user', function() end)
+    assert.is_not_nil(sort.get('user'))
+    assert.is_not_nil(sort.get('User'))
   end)
 end)
