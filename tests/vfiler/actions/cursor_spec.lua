c19f3ef3dca9c5e6c640813c45c2c42d62fecd1f
th.obaland@gmail.com <th.obaland@gmail.com>
2022-06-16 19:39:15 +0900

    Add test code.

--- tests/vfiler/actions/cursor_spec.lua
+++ tests/vfiler/actions/cursor_spec.lua
@@ -5,9 +5,13 @@ describe('cursor actions', function()
   local vfiler = u.vfiler.start(u.vfiler.generate_options())
   local action_sequence = {
     move_cursor_down = a.move_cursor_down,
+    move_cursor_down_sibling = a.move_cursor_down_sibling,
     move_cursor_up = a.move_cursor_up,
+    move_cursor_up_sibling = a.move_cursor_up_sibling,
     move_cursor_bottom = a.move_cursor_bottom,
+    move_cursor_bottom_sibling = a.move_cursor_bottom_sibling,
     move_cursor_top = a.move_cursor_top,
+    move_cursor_top_sibling = a.move_cursor_top_sibling,
     loop_cursor_up = a.loop_cursor_up,
     loop_cursor_down = a.loop_cursor_down,
   }
