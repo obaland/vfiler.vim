65371fee51c283253ec057dbc0066c3aa2155858
amedama41 <kamo.devel41@gmail.com>
2023-09-10 19:05:58 +0900

    fix: git status parse error for large stdout

--- lua/vfiler/libs/async/jobs/job_nvim.lua
+++ lua/vfiler/libs/async/jobs/job_nvim.lua
@@ -24,9 +24,19 @@ function Job:start(command, options)
       end
     end,
   }
+  local data_buffer = ''
   if options.on_received then
     jobopts.on_stdout = function(channel, datas, name)
-      for _, data in ipairs(datas) do
+      datas[1] = data_buffer .. datas[1]
+      local data_len = #datas
+      if datas[data_len] ~= '' then
+        data_buffer = datas[data_len]
+        data_len = data_len - 1
+      else
+        data_buffer = ''
+      end
+      for i = 1, data_len do
+        local data = datas[i]
         if #data > 0 then
           options.on_received(self, data)
         end
@@ -35,6 +45,9 @@ function Job:start(command, options)
   end
   if options.on_completed then
     jobopts.on_exit = function(id, code, event)
+      if options.on_received and data_buffer ~= '' then
+        options.on_received(self, data_buffer)
+      end
       options.on_completed(self, code)
     end
   end
