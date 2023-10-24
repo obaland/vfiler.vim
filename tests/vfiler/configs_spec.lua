f82d10381b76fae8297b4e2f75ffa4c318ab7402
th.obaland@gmail.com <th.obaland@gmail.com>
2023-10-24 16:35:39 +0900

    change: unit test codes.

--- tests/vfiler/configs_spec.lua
+++ tests/vfiler/configs_spec.lua
@@ -13,22 +13,6 @@ else
   }
 end
 
-local function eq(excepted, actual)
-  assert.is_equal(excepted, actual)
-end
-local function is_nil(actual)
-  assert.is_nil(actual)
-end
-local function is_not_nil(actual)
-  assert.is_not_nil(actual)
-end
-local function is_false(actual)
-  assert.is_false(actual)
-end
-local function is_true(actual)
-  assert.is_true(actual)
-end
-
 describe('parse command args', function()
   local parse_options = config.parse_options
 
@@ -36,70 +20,70 @@ describe('parse command args', function()
     for _, paths in ipairs(pathpairs) do
       local args = '-auto-cd ' .. paths.input
       local options, path = parse_options(args)
-      eq(paths.output, path)
-      is_true(options.auto_cd)
+      assert.is_equal(paths.output, path)
+      assert.is_true(options.auto_cd)
     end
   end)
 
   it('empty', function()
     local options = parse_options('')
-    is_not_nil(options)
+    assert.is_not_nil(options)
   end)
 
   it('dplicated paths', function()
     for _, paths in ipairs(pathpairs) do
       local args = paths.input .. ' ' .. paths.input
-      is_nil(parse_options(args))
+      assert.is_nil(parse_options(args))
     end
     local options = parse_options('')
-    is_not_nil(options)
+    assert.is_not_nil(options)
   end)
 
   it('key-value option', function()
     local args = '-name="Test Name" -columns=indent,name,size'
     local options = parse_options(args)
-    eq('Test Name', options.name)
-    eq('indent,name,size', options.columns)
+    assert.is_equal('Test Name', options.name)
+    assert.is_equal('indent,name,size', options.columns)
   end)
 
   it('illegal key-value option', function()
     local args = '-name'
     local options = parse_options(args)
-    is_nil(options)
+    assert.is_nil(options)
 
     args = '-name='
     options = parse_options(args)
-    is_nil(options)
+    assert.is_nil(options)
   end)
 
   it('flag option', function()
     local args = '-auto-cd -listed'
     local options = parse_options(args)
-    is_true(options.auto_cd)
-    is_true(options.listed)
+    assert.is_true(options.auto_cd)
+    assert.is_true(options.listed)
 
     args = '-no-auto-cd -no-listed'
     options = parse_options(args)
-    is_false(options.auto_cd)
-    is_false(options.listed)
+    assert.is_false(options.auto_cd)
+    assert.is_false(options.listed)
   end)
 
   it('illegal flag option', function()
     local args = '-auo-cd'
     local options = parse_options(args)
-    is_nil(options)
+    assert.is_nil(options)
 
     args = '-auto-cd=test'
     options = parse_options(args)
-    is_nil(options)
+    assert.is_nil(options)
   end)
 
   it('nested option', function()
     local args = '-name="Test Name" -auto-cd -git-enabled -no-git-untracked'
     local options = parse_options(args)
-    eq('Test Name', options.name)
-    is_true(options.auto_cd)
-    is_true(options.git.enabled)
-    is_false(options.git.untracked)
+    assert.is_equal('Test Name', options.name)
+    assert.is_true(options.auto_cd)
+    assert.is_true(options.git.enabled)
+    assert.is_false(options.git.untracked)
   end)
 end)
